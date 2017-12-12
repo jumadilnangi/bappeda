1060b54ce43b10910958d9234ffb5fc9fa68bcfc
Yusuf Ayuba <virbo80@yahoo.com>
2017-12-12 21:10:28 +0800

    #27, perbaikan input kegiatan

--- backend/views/mb-kegiatan/_form.php
+++ backend/views/mb-kegiatan/_form.php
@@ -5,8 +5,12 @@ use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
 use yii\web\JsExpression;
 
-use kartik\select2\Select2;
 use kartik\widgets\ActiveForm;
+use kartik\select2\Select2;
+use kartik\widgets\DepDrop;
+
+use backend\models\MbSkpd;
+
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\MbKegiatan */
@@ -24,41 +28,73 @@ $form = ActiveForm::begin(['type' => ActiveForm::TYPE_HORIZONTAL]);
         </div>
     </div>
     <div class="box-body">
-        <!--?= $form->field($model, 'mb_program_id')->textInput() ?-->
         <?php
             $_value['name'] = '';
             if (isset($model->mb_program_id)) {
-                $_value = Yii::$app->db->createCommand("SELECT mb_program_id AS id, 
-                            CONCAT(mb_program_kode,' - ',mb_program_nama) AS name
-                        FROM mb_program
-                        WHERE mb_program_id = :id")
-                    ->bindValue(':id', $model->mb_program_id)
+                $_value = Yii::$app->db->createCommand("SELECT mb_skpd_id AS id, 
+                            mb_skpd_nama AS name
+                        FROM mb_skpd
+                        WHERE mb_skpd_id = :id")
+                    ->bindValue(':id', $model->mbProgram->mbUrusanHasSkpd->mbSkpd->mb_skpd_id)
                     ->queryOne();
-
             }
-            echo $form->field($model, 'mb_program_id')->widget(Select2::classname(),[
+            $modelSkpd->mb_skpd_id = $_value ? $_value['name'] : $modelSkpd->mb_skpd_id;
+            echo $form->field($modelSkpd, 'mb_skpd_id')->widget(Select2::classname(),[
+                'theme' => Select2::THEME_BOOTSTRAP,
+                //'initValueText' => $_opd,
+                'options' => [
+                    'placeholder' => 'Pilih OPD..',
+                    'id' => 'id_skpd',
+                ],
+                'pluginOptions' => [
+                    //'width' => '150px',
+                    'allowClear' => true,
+                    'language' => [
+                        'errorLoading' => new JsExpression("function () { return 'Waiting for results...'; }"),
+                    ],
+                    'ajax' => [
+                        'url' => Url::to(['data/getskpd']),
+                        'type' => 'POST',
+                        'dataType' => 'json',
+                        'delay' => 20,
+                        'data' => new JsExpression('function(cari){return { cari: cari.term, page: 30 }}'),
+                        'cache' => 'true'
+                    ],
+                ],
+            ])->label('OPD');
+
+            //dependropdown
+            $_value['id'] = '';
+            $_value['name'] = '';
+            if (isset($model->mb_program_id)) {
+                $_value = Yii::$app->db->createCommand("SELECT prog.mb_program_id AS id,
+                            CONCAT(prog.mb_program_kode,' - ', prog.mb_program_nama) AS name
+                        FROM mb_kegiatan AS keg
+                        JOIN mb_program AS prog USING(mb_program_id)
+                        JOIN mb_urusan_has_skpd AS hskpd USING(mb_urusan_has_skpd_id)
+                        JOIN mb_skpd AS skpd USING(mb_skpd_id)
+                        WHERE keg.mb_kegiatan_id = :id")
+                    ->bindValue(':id', $model->mb_kegiatan_id)
+                    ->queryOne();
+            }
+            echo $form->field($model, 'mb_program_id')->widget(DepDrop::classname(), [
+                'type' => DepDrop::TYPE_SELECT2,
+                'data' => [$_value['id'] => $_value['name']],
+                'options' => ['id' => 'id_program'],
+                'select2Options'=>[
                     'theme' => Select2::THEME_BOOTSTRAP,
-                    'initValueText' => $_value['name'],
-                    'options' => [
-                        'placeholder' => 'Program..',
-                        //'id' => 'id_program',
-                    ],
-                    'pluginOptions' => [
-                        //'width' => '500px',
-                        'allowClear' => true,
-                        'language' => [
-                            'errorLoading' => new JsExpression("function () { return 'Waiting for results...'; }"),
-                        ],
-                        'ajax' => [
-                            'url' => Url::to(['data/getprogram']),
-                            'type' => 'POST',
-                            'dataType' => 'json',
-                            'delay' => 20,
-                            'data' => new JsExpression('function(cari){return { cari: cari.term, page: 30 }}'),
-                            'cache' => 'true'
-                        ],
-                    ],
-                ]);
+                    'options' => ['placeholder' => 'Pilih Program...'],
+                    'pluginOptions'=>[
+                        'allowClear'=>true,
+                        //'width' => '400px'
+                    ]
+                ],
+                'pluginOptions'=>[
+                    'depends'=>['id_skpd'],
+                    'url' => Url::to(['subprogram']),
+                    'loadingText' => 'Mengambil data Program...',
+                ]
+            ]);
         ?>
 
         <?= $form->field($model, 'mb_kegiatan_kode')->textInput(['maxlength' => true]) ?>
